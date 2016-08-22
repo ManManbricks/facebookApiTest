42ff37d1fb3ea7050c1c1559f708df50f83c3f22
ManManBricks <chika.callis@gmail.com>
2016-08-22 15:07:10 +0100

    First commit on test.js

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 var readline = require('readline');
 var rl = readline.createInterface({
+	
   input: process.stdin,
   output: process.stdout
 });
-var Twit = require('twit')
-
+var Twit = require('twit');
 var T = new Twit({
+	
   consumer_key:         'Bz3cplaIPPVLWb0JcagmMKBkb',
   consumer_secret:      'BIc4XfpnID4wXMnPILJ1FIHmHSxtpQiM9ysDjP2fKYHNACGxMJ',
   access_token:         '185426580-xNTOHxpq1hrRLmGFomFnce7kOaypVZLHgVUFIyfm',
   access_token_secret:  'Y9tDNAYsW0NRkWYHlMtEMS2rBKbkJY8JPCUoa7phryW3o',
   timeout_ms:           60*1000,  
-})
+});
 
 console.log( "What do you want to do?" );
 console.log( "Enter 1 to post a comment" );
@@ -21,16 +22,15 @@ rl.prompt();
 
 rl.on('line', (line) => {
 
-
 	switch(line.trim()) {
+		
     case "1":
       rl.question('Enter a post : ', function(answer){
              
 			T.post('statuses/update', { status: answer }, function(err, data, response) {
 			console.log(data)
-			})
-		})
-		
+			});
+		});		
     break;
 	case "2":
 	   rl.question("Enter word to search : ", function(word){
@@ -40,10 +40,10 @@ rl.on('line', (line) => {
 				   var text = word + " since:" + date;  
 					T.get('search/tweets', { q: text, count: limit }, function(err, data, response) {
 						 console.log(data)
-					})
-			   })
-			})	 
-       })
+					});
+			   });
+			});	 
+       });
 	break;
 	case "exit": rl.close(); break;
     default:
