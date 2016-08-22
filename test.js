87d6f8b45b6cb59d029c5d4b04cb9b37ebfce12e
ManManBricks <chika.callis@gmail.com>
2016-08-22 14:08:17 +0100

    First commit on test.js

--- test.js
+++ test.js
@@ -6,10 +6,10 @@ var rl = readline.createInterface({
 var Twit = require('twit')
 
 var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_key:         'Bz3cplaIPPVLWb0JcagmMKBkb',
+  consumer_secret:      'BIc4XfpnID4wXMnPILJ1FIHmHSxtpQiM9ysDjP2fKYHNACGxMJ',
+  access_token:         '185426580-xNTOHxpq1hrRLmGFomFnce7kOaypVZLHgVUFIyfm',
+  access_token_secret:  'Y9tDNAYsW0NRkWYHlMtEMS2rBKbkJY8JPCUoa7phryW3o',
   timeout_ms:           60*1000,  
 })
 
