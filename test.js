5706737c350e31882356855bdd513b5cfd8826f5
ManManBricks <chika.callis@gmail.com>
2016-08-22 18:21:51 +0100

    Revised Code

--- test.js
+++ test.js
@@ -14,10 +14,16 @@ var T = new Twit({
   timeout_ms:           60*1000,  
 });
 	
+var ques = function(){
+console.log("\n\n");
 console.log( "What do you want to do?" );
 console.log( "Enter 1 to post a comment" );
 console.log( "Enter 2 to search twitter for all tweets" );
+console.log( "Enter 3 to get list of user id's that follow a particular screen name" );
 console.log( "Press exit to exit app" );
+};
+
+ques();
 rl.prompt();
 
 rl.on('line', (line) => {
@@ -30,8 +36,10 @@ rl.on('line', (line) => {
             
 			T.post('statuses/update', { status: answer }, function(err, data, response) {
 			console.log(data);
+			ques();
 			});
-		});		
+		});	
+ 		
     break;
 	case "2":
 	   rl.question("Enter word to search : ", function(word){
@@ -41,22 +49,29 @@ rl.on('line', (line) => {
 				   var text = word + " since:" + date;  
 					T.get('search/tweets', { q: text, count: limit }, function(err, data, response) {
 						 console.log(data)
+						 ques();
 					});
 			   });
 			});	 
        });
 	break;
+	
+	case "3": 
+		rl.question("Enter screen name (e.g. dave) : ", function(name){
+		 
+				T.get('followers/ids', { screen_name: name },  function (err, data, response) {
+					console.log(data)
+					ques();
+				})
+		});
+		break;
+	 
 	case "exit": rl.close(); break;
     default:
       console.log(`Invalid option '${line.trim()}'`);
       break;
   }
-  /*console.log( "What do you want to do?" );
-console.log( "Enter 1 to post a comment" );
-console.log( "Enter 2 to search twitter for all tweets" );
-console.log( "Press exit to exit app" );
-  rl.prompt();*/
-  
+ 
   }).on('close', () => {
   console.log('Thank You for using my app');
   console.log( "Exiting app..." );
