a834ea39b76ad52a508f5805da541e0c55f5efb7
ManManBricks <chika.callis@gmail.com>
2016-08-22 13:19:00 +0100

    First commit on test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 var readline = require('readline');
 var request = require('request');
+var api = require('./facebook');
+var oauth = require('./oauth')
+ // app = express();
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -7,7 +10,7 @@ var rl = readline.createInterface({
 });
 
 var message = "";
-var access_token = "";
+var access_token = "595375967308784|YFmEHJfn4rVdif8KHpQW7Yxl17g";
 var url = ""; //'https://graph.facebook.com/me/feed';
 var response = "";
 
@@ -44,14 +47,37 @@ rl.question('Enter a message : ', function(answer){
   rl.question('Specify the URL and query string parameters needed for the request : ', function(answer){
  
   url = answer;
-});
+  });
 rl.close();
 });
 
 
 
 
+// Setup middleware
+app.use(express.static(__dirname));
+app.use(express.bodyParser());
+
+app.post('/post', function(req, res) {
+  // Check to ensure user has a valid access_token
+  if (oauth.access_token) {
+
+    // Call function that contains API call to post on Facebook (see facebook.js)
+    api.postMessage(oauth.access_token, req.body.message, res);
+    
+  } else {
+    console.log("Couldn't confirm that user was authenticated. Redirecting to /");
+    res.redirect('/');
+  }
+});
+
+// Routes for OAuth calls
+app.get('/login', oauth.login);
+app.get('/callback', oauth.callback);
+
+app.listen(80);
 
 
 
-exports.postMessage = postMessage;
+
+//exports.postMessage = postMessage;
