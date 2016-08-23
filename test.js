172cf4350332bd7d6fe189627d89dd28d2a55304
ManManBricks <chika.callis@gmail.com>
2016-08-23 11:55:07 +0100

    Revised Code

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var readline = require('readline');
 var rl = readline.createInterface({
 	
@@ -7,10 +8,10 @@ var rl = readline.createInterface({
 var Twit = require('twit');
 var T = new Twit({
 	
-  consumer_key:         'Bz3cplaIPPVLWb0JcagmMKBkb',
-  consumer_secret:      'BIc4XfpnID4wXMnPILJ1FIHmHSxtpQiM9ysDjP2fKYHNACGxMJ',
-  access_token:         '185426580-xNTOHxpq1hrRLmGFomFnce7kOaypVZLHgVUFIyfm',
-  access_token_secret:  'Y9tDNAYsW0NRkWYHlMtEMS2rBKbkJY8JPCUoa7phryW3o',
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token:  process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
   timeout_ms:           60*1000,  
 });
 	
